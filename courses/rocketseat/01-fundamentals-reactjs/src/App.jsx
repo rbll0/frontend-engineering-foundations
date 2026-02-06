69a747761204f110eb57ddd1289a6819101b1717
Gustavo Rabelo Frere <148499706+rbll0@users.noreply.github.com>
2026-02-06 11:23:46 -0300

    ✨ feat(courses): initialize React project with Vite

---
+++ courses/rocketseat/01-fundamentals-reactjs/src/App.jsx
@@ -0,0 +1,16 @@
+import { Post } from './Post'
+
+
+export default function App() {
+  return (
+    <div>
+      <Post
+        author="Gustavo Rabelo Frere"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat exercitationem perspiciatis quo dolorem necessitatibus numquam ipsam ratione libero reiciendis quibusdam in molestiae iusto excepturi rem aliquam, fuga consequuntur quasi quaerat." />
+
+      <Post
+        author="Esther Rabelo"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat exercitationem perspiciatis quo dolorem necessitatibus numquam ipsam ratione libero reiciendis quibusdam in molestiae iusto excepturi rem aliquam, fuga consequuntur quasi quaerat." />
+    </div>
+  )
+}
