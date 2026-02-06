c16edd2f0c5fcd3e7e0cd0bf1cbb78ef335d9bfe
Gustavo Rabelo Frere <148499706+rbll0@users.noreply.github.com>
2026-02-06 12:50:25 -0300

    ✨ feat (courses): add Header component with global styles

--- courses/rocketseat/01-fundamentals-reactjs/src/App.jsx
+++ courses/rocketseat/01-fundamentals-reactjs/src/App.jsx
@@ -1,9 +1,12 @@
+import { Header } from './components/Header'
 import { Post } from './Post'
 
+import "./global.css"
 
 export default function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Gustavo Rabelo Frere"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat exercitationem perspiciatis quo dolorem necessitatibus numquam ipsam ratione libero reiciendis quibusdam in molestiae iusto excepturi rem aliquam, fuga consequuntur quasi quaerat." />
